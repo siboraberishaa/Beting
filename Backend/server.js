a6da7e50804e6cdc37da9f0e597bcd07346d51c7
sibora github <siboraberisha@gmail.com>
2024-06-10 15:00:42 +0200

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -51,11 +51,9 @@ app.use("/api/transfers", transferRoutes);
 // app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-    );
+    app.get("/", (req, res) => {
+        res.send("API is running....");
+    });
 } else {
     app.get("/", (req, res) => {
         res.send("API is running....");
