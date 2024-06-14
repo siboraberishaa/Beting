ab17151d00d1a66555342f3389f1c7d2273b59c8
sibora github <siboraberisha@gmail.com>
2024-06-14 22:31:20 +0200

    commit599595995

--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,28 @@
 import path from "path";
 import express from "express";
-import cors from 'cors'
+import cors from 'cors';
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
-// import productRoutes from "./routes/productRoutes.js";
-// import categoryRoutes from "./routes/categoryRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-// import uploadRoutes from "./routes/uploadRoutes.js";
-// import partnerRoutes from "./routes/partnerRoutes.js";
-// import contactRoutes from "./routes/contactRoutes.js";
-// import newsletterRoutes from "./routes/newsletterRoutes.js";
 import transferRoutes from './routes/transferRoutes.js';
-import rolesRoutes from './routes/rolesRoutes.js'
-import ticketRoutes from './routes/ticketRoutes.js'
-import financeRoutes from './routes/financeRoutes.js'
-// import cron from 'node-cron';
-// import { backUpDB } from "./backupDB.js";
+import rolesRoutes from './routes/rolesRoutes.js';
+import ticketRoutes from './routes/ticketRoutes.js';
+import financeRoutes from './routes/financeRoutes.js';
 
 connectDB();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors({
+const corsOptions = {
     origin: 'https://website-665aeeac.ameba-rks.com', // replace with your actual origin
     credentials: true,
-  }));
-  
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 // Body parser middleware
 app.use(express.json());
@@ -38,20 +32,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Your routes here
-// app.use("/api/products", productRoutes);
-// app.use("/api/category", categoryRoutes);
-// app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/roles", rolesRoutes);
 app.use("/api/tickets", ticketRoutes);
 app.use("/api/transfers", transferRoutes);
 app.use("/api/finances", financeRoutes);
-// app.use("/api/partners", partnerRoutes);
-// app.use("/api/contact", contactRoutes);
-// app.use("/api/newsletter", newsletterRoutes);
-
-// const __dirname = path.resolve(); //set __dirname to current directory
-// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
     app.get("/", (req, res) => {
@@ -63,4 +48,4 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
--- Frontend/src/features/apiSlice.js
+++ Frontend/src/features/apiSlice.js
@@ -3,6 +3,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: ''}),
+  prepareHeaders: (headers) => {
+    headers.set('Content-Type', 'application/json');
+    return headers;
+  },
   tagTypes: ['Roles',],
   endpoints: (builder) => ({
     getInPlayFilter: builder.query({  
