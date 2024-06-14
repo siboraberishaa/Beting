2e69243463e0aa10d2efea1b29c4a9bca0f7a2f2
sibora github <siboraberisha@gmail.com>
2024-06-14 23:25:57 +0200

    mlk

--- Backend/server.js
+++ Backend/server.js
@@ -1,51 +1,50 @@
-import path from "path";
-import express from "express";
+import path from 'path';
+import express from 'express';
 import cors from 'cors';
-import dotenv from "dotenv";
-dotenv.config();
-import connectDB from "./config/db.js";
-import cookieParser from "cookie-parser";
-import userRoutes from "./routes/userRoutes.js";
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 import transferRoutes from './routes/transferRoutes.js';
 import rolesRoutes from './routes/rolesRoutes.js';
 import ticketRoutes from './routes/ticketRoutes.js';
 import financeRoutes from './routes/financeRoutes.js';
 
+dotenv.config();
 connectDB();
 
-const port = process.env.PORT || 5000;
 const app = express();
+const port = process.env.PORT || 5000;
 
-const corsOptions = {
+// Add cache-control header middleware
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
+  next();
+});
+
+app.use(cors({
     origin: ['https://website-665aeeac.ameba-rks.com', 'https://666cb2f5e04051d4e29de3a6--admirable-creponne-e5f66c.netlify.app'], // replace with your actual origin
-    credentials: true,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+  credentials: true, // Include cookies in cross-origin requests
+}));
 
-app.use(cors(corsOptions));
-
-// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Cookie parser middleware
 app.use(cookieParser());
 
-// Your routes here
-app.use("/api/users", userRoutes);
-app.use("/api/roles", rolesRoutes);
-app.use("/api/tickets", ticketRoutes);
-app.use("/api/transfers", transferRoutes);
-app.use("/api/finances", financeRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/roles', rolesRoutes);
+app.use('/api/tickets', ticketRoutes);
+app.use('/api/transfers', transferRoutes);
+app.use('/api/finances', financeRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.get("/", (req, res) => {
-        res.send("API is running....");
-    });
+if (process.env.NODE_ENV === 'production') {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
 } else {
-    app.get("/", (req, res) => {
-        res.send("API is running....");
-    });
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
 }
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
