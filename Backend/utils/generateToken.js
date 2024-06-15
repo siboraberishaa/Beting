80bdc8f398d4561ef1ffd10cee6251f7168bbf12
sibora github <siboraberisha@gmail.com>
2024-06-15 09:43:57 +0200

    cmt

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -16,6 +16,7 @@ const generateToken = (res, userId) => {
     secure: process.env.NODE_ENV !== 'development',
     sameSite: 'none',
     domain: '.ameba-rks.com', // Match the domain used when setting the cookie
+    path: '/',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
