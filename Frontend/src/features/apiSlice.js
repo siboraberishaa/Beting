224096074d4aa58549c2b0d74a5b0bf01609a3f4
sibora github <siboraberisha@gmail.com>
2024-06-15 14:19:52 +0200

    v8

--- Frontend/src/features/apiSlice.js
+++ Frontend/src/features/apiSlice.js
@@ -13,7 +13,7 @@ export const apiSlice = createApi({
       query: () => ({
         url: 'https://bet365-api-inplay.p.rapidapi.com/bet365/get_sports',
         headers: {
-          'X-RapidAPI-Key': '0a1732f5efmshb425d8b87e4606fp19466ajsn7a5ac2b6d89e',
+          'X-RapidAPI-Key': 'b257ce1b5fmsh45c65f391861a25p1cd103jsneae8f4edf029',
           'X-RapidAPI-Host': 'bet365-api-inplay.p.rapidapi.com'
         },
       }),
@@ -22,7 +22,7 @@ export const apiSlice = createApi({
       query: (sport) => ({
         url: `https://bet365-api-inplay.p.rapidapi.com/bet365/get_sport_events/${sport}`,  
         headers: {
-          'X-RapidAPI-Key': '0a1732f5efmshb425d8b87e4606fp19466ajsn7a5ac2b6d89e',
+          'X-RapidAPI-Key': 'b257ce1b5fmsh45c65f391861a25p1cd103jsneae8f4edf029',
           'X-RapidAPI-Host': 'bet365-api-inplay.p.rapidapi.com'
         },
       }),
@@ -31,7 +31,7 @@ export const apiSlice = createApi({
       query: (eventId) => ({
         url: `https://bet365-api-inplay.p.rapidapi.com/bet365/get_event_with_markets/${eventId}`,  
         headers: {
-          'X-RapidAPI-Key': '0a1732f5efmshb425d8b87e4606fp19466ajsn7a5ac2b6d89e',
+          'X-RapidAPI-Key': 'b257ce1b5fmsh45c65f391861a25p1cd103jsneae8f4edf029',
           'X-RapidAPI-Host': ' bet365-api-inplay.p.rapidapi.com'
         },
       }),
